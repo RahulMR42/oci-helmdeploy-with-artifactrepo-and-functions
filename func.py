6626744871c2b9fd676a333bcb3cd5276a9b83cb
RahulMR <rahulmasquee@gmail.com>
2021-12-27 19:12:23 +0530

    New changes

--- func.py
+++ func.py
@@ -3,22 +3,28 @@
 #
 
 import io
+import os
 import json
 import logging
+
+import oci
 
 from fdk import response
 
 
 def handler(ctx, data: io.BytesIO=None):
-    body = json.loads(data.getvalue()) 
-    logging.getLogger().info("Fetching the information")
-    artifact_repo_id = body[0]['data']['stateChange']['current']['repositoryId']
-    artifact_path = body[0]['data']['stateChange']['current']['artifactPath']
-    artifact_version = body[0]['data']['stateChange']['current']['version']
-    logging.getLogger().info(f'Input Params Repo = {artifact_repo_id} Path = {artifact_path}, Version = {artifact_version}')
-    
-    return response.Response(
-        ctx, 
-        response_data=json.dumps({"status": "Hello World! with customImage"}),
-        headers={"Content-Type": "application/json"}
-    )
+    try:
+        body = json.loads(data.getvalue()) 
+        logging.getLogger().info("Fetching the information")
+        artifact_repo_id = body[0]['data']['stateChange']['current']['repositoryId']
+        artifact_path = body[0]['data']['stateChange']['current']['artifactPath']
+        artifact_version = body[0]['data']['stateChange']['current']['version']
+        signer = oci.auth.signers.get_resource_principals_signer()
+        os.environ['OCI_CLI_AUTH']="resource_principal" #set OCI CLI to use resource_principal authorization
+        logging.getLogger().info(f'Input Params Repo = {artifact_repo_id} Path = {artifact_path}, Version = {artifact_version}')
+        return response.Response(
+            ctx, 
+            response_data=json.dumps({"status": "Hello World! with customImage"}),
+            headers={"Content-Type": "application/json"})
+    except Exception as error:
+        logging.getLogger().info(f'Exception - {error}')
